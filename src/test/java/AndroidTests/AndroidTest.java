9f38d98a348642b7f2a1fcc65a09e3b44184bd6a
Amit Kumar Santra <mailme.amitsantra@gmail.com>
2021-05-04 12:10:44 +0530

    Refactror and structural change

---
+++ src/test/java/AndroidTests/AndroidTest.java
@@ -0,0 +1,13 @@
+package AndroidTests;
+import Base.BaseTest;
+
+import org.testng.annotations.Test;
+
+public class AndroidTest extends BaseTest {
+
+    @Test
+    public void MyFirstTest()
+    {
+        System.out.println("Test1");
+    }
+}
--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -1,35 +1,35 @@
+package Base;
+
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Test;
 
 import java.io.File;
 import java.net.URL;
 
 public class BaseTest {
-
+    AndroidDriver<AndroidElement> driver = null;
     @BeforeSuite
     public void setUp() {
         File src = new File("src");
         File demoApk = new File(src, "ApiDemos-debug.apk");
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "AmitEmulator");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "AmitEmulator:5554");
         capabilities.setCapability(MobileCapabilityType.APP, demoApk.getAbsolutePath());
         try {
-        AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wb/hub"), capabilities);
+        driver = new AndroidDriver<>(new URL("http://127.0.1.1:4723/wd/hub"), capabilities);
         }
         catch (Exception e){
             e.printStackTrace();
         }
-
     }
 
-    @Test
-    public void MyFirstTest()
-    {
-        System.out.println("Test1");
+    @AfterSuite
+    public void testTearDown(){
+        driver.quit();
     }
-
 }
