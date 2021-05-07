c112f6627eef6dd8f3ee4f763c4dfcea750b45a1
Amit Kumar Santra <mailme.amitsantra@gmail.com>
2021-05-07 15:22:51 +0530

    verifyNavigationByMenuItem - refactor

--- src/test/java/AndroidTests/AndroidTest.java
+++ src/test/java/AndroidTests/AndroidTest.java
@@ -1,29 +1,25 @@
 package AndroidTests;
 import Base.BaseTest;
-
-import io.appium.java_client.android.AndroidElement;
 import org.testng.annotations.Test;
 
 public class AndroidTest extends BaseTest {
-
     @Test
-    public void verifyNavigationByMenuItem() throws InterruptedException {
-         /*
-         Xpath Writing syntax
-         //tagName[@attribute='value']
-         //android.widget.TextView[@attribute='']
-          */
-        driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
-        Thread.sleep(1000);
-        driver.findElementByXPath("//android.widget.TextView[@text='3. Preference dependencies']").click();
-        Thread.sleep(1000);
-        driver.findElementByClassName("android.widget.CheckBox").click();
-        Thread.sleep(1000);
-        driver.findElementByXPath("//android.widget.TextView[@text='WiFi settings']").click();
-        Thread.sleep(1000);
-        driver.findElementByClassName("android.widget.EditText").sendKeys("Hello Appium");
-        Thread.sleep(1000);
-        driver.findElementByXPath("//android.widget.Button[@text='OK']").click();
-        Thread.sleep(1000);
+    public void verifyNavigationByMenuItem() {
+        try {
+            driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
+            Thread.sleep(1000); // static wait, later modify with dynamic wait
+            driver.findElementByXPath("//android.widget.TextView[@text='3. Preference dependencies']").click();
+            Thread.sleep(1000);
+            driver.findElementByClassName("android.widget.CheckBox").click();
+            Thread.sleep(1000);
+            driver.findElementByXPath("//android.widget.TextView[@text='WiFi settings']").click();
+            Thread.sleep(1000);
+            driver.findElementByClassName("android.widget.EditText").sendKeys("Hello Appium");
+            Thread.sleep(1000);
+            driver.findElementByXPath("//android.widget.Button[@text='OK']").click();
+            Thread.sleep(1000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
     }
 }
