dacc336f85df28ac37d995b13cf63e0016132a9e
Amit Kumar Santra <mailme.amitsantra@gmail.com>
2021-05-08 22:52:24 +0530

    NavigationByUiAutomator

--- src/test/java/AndroidTests/AndroidTest.java
+++ src/test/java/AndroidTests/AndroidTest.java
@@ -1,5 +1,6 @@
 package AndroidTests;
 import Base.BaseTest;
+import io.appium.java_client.android.AndroidElement;
 import org.testng.annotations.Test;
 
 public class AndroidTest extends BaseTest {
@@ -22,4 +23,31 @@ public class AndroidTest extends BaseTest {
             e.printStackTrace();
         }
     }
+
+
+    //Using UiAutomator
+    @Test
+    public void verifyNavigationByUiAutomator(){
+        try{
+            driver.findElementByAndroidUIAutomator("text(\"Views\")").click();
+            Thread.sleep(1000);
+            driver.findElementByAndroidUIAutomator("text(\"Animation\")").click();
+            Thread.sleep(1000);
+            AndroidElement transc =  driver.findElementByAndroidUIAutomator("text(\"3D Transition\")");
+            if (transc.isEnabled())
+                transc.click();
+            Thread.sleep(1000);
+            int size = driver.findElementsByAndroidUIAutomator("new UiSelector().enabled(true)").size();
+            System.out.println(size);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void verifyMobileGesture(){
+
+    }
+
+
 }
