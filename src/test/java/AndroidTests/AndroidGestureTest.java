05ce01a28e2088a69544ce64d37b475944517b9a
Amit Kumar Santra <mailme.amitsantra@gmail.com>
2021-05-16 12:15:25 +0530

    verifyMobileGestureDoubleTap - refactor

--- src/test/java/AndroidTests/AndroidGestureTest.java
+++ src/test/java/AndroidTests/AndroidGestureTest.java
@@ -4,6 +4,7 @@ import io.appium.java_client.MultiTouchAction;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.touch.offset.PointOption;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
@@ -172,6 +173,8 @@ public class AndroidGestureTest extends BaseTest {
             TouchAction<?> touchAction = new TouchAction<>(driver);
             touchAction.tap(tapOptions().withElement(element(nextCount)).withTapsCount(2)).perform();
             Thread.sleep(2000);
+            String count = driver.findElementByAndroidUIAutomator("text(\"2\")").getText();
+            Assert.assertEquals(2, Integer.parseInt(count), "Double tap not happened");
         }catch (Exception e){
             e.printStackTrace();
         }
